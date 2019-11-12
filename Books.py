d4b2257d3775e43aa8df1e712c43c6cce2dfd171
Bruno Eduardo <bruno97br@gmail.com>
2019-11-12 16:33:44 -0300

    Create Book class, w/ national and international books

---
+++ Books.py
@@ -0,0 +1,36 @@
+from forex_python.converter import CurrencyRates
+
+class Book:
+    #args = (   unidade, seq, author, title, publisher, vol, year, edition, ISBN10, ISBN13, priority, quantity
+    #           unitValue, totalValue, observation, dollarFlag, unitValueWithTAxes, totalValuewithTaxesBRL, dollarPrice):
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
+            #expecting  unidade, seq, author, title, publisher, vol, year, edition, ISBN10, ISBN13, priority, quantity,
+            #           unitValue, observation
+        self.totalValue = round(self.getTotalValue(),2)
+
+    def getTotalValue(self):
+        return self.unitValue*self.quantity
+
+    def __str__(self):
+        out = ""
+        out += "This is a book. Attrs:"
+        out += str(vars(self))
+        return out
+
+class nationalBook(Book):
+    def __init__(self, **kwargs):
+        self.dollarFlag = False
+        super(nationalBook, self).__init__(**kwargs)
+
+class internationalBook(Book):
+    def __init__(self, **kwargs):
+        self.dollarFlag = True
+        self.dollarPrice = round(CurrencyRates().get_rate('USD', 'BRL'), 2)
+        self.taxes = 40/100
+        self.dolarRateWithTaxes = self.dollarPrice*(1+self.taxes)
+        super(internationalBook, self).__init__(**kwargs)
+
+    #@overides from Book.getTotalValue(self)
+    def getTotalValue(self):
+        return self.dolarRateWithTaxes*super(internationalBook, self).getTotalValue()
--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
 from datetime import date
-
 from openpyxl import load_workbook
 
+from Books import nationalBook, internationalBook
 
 # Global vars
 urlToParse = u"https://www.amazon.com.br/Resist%C3%AAncia-dos-materiais-R-Hibbeler-ebook/dp/B07WH4677K/ref=sr_1_1?qid=1573499147&refinements=p_27%3AR.+C.+Hibbeler&s=digital-text&sr=1-1"
@@ -16,11 +16,12 @@ DEBUG_FLAG = False
 
 class Request:
     def __init__(self):
-        self.library = None
+        self.library    = None
         self.branchLine = None
-        self.email = None
-        self.email = None
-        self.date = None
+        self.email      = None
+        self.email      = None
+        self.date       = None
+        self.books      = []
 
     def getMetaData(self):
         self.library        = input("Insira o nome da Biblioteca: ")
@@ -33,11 +34,15 @@ class Request:
         #TODO implement
         pass
 
+    def appendBook(self, book):
+        self.books.append(book)
+
     def __str__(self):
         out = ""
         out += "This is a resquest. Attrs:"
         out += str(vars(self))
         return out
+
 
 def openUrlAndSoup(url):
     #TODO catch para urllib.error.URLError
@@ -61,10 +66,33 @@ def main():
     #print(request)
 
     wb = load_workbook(originalSpeadsheet)
-    #print(wb.sheetnames)
+    print(wb.sheetnames)
 
-    #soup = openUrlAndSoup(urlToParse)
-    #anchorAndGet(soup)
+    brBook = nationalBook(unidade='FEM', seq=1, author='Machado', title='Dom Casmurro', publisher="Unicamp", vol=1,
+                          year=1899, edition=1, ISBN10="?"*10, ISBN13="?"*13, priority=1, quantity=1, unitValue=20,
+                          observation="LivroBR")
+
+    euaBook = internationalBook(unidade='FEM', seq=1, author='J. K. Rowling', title='Harry Potter', publisher="Rocco",
+                                vol=1, year=1997, edition=1, ISBN10="!"*10, ISBN13="!"*13, priority=2, quantity=1,
+                                unitValue=20, dollarFlag=True, observation="Livro Gringo")
+
+    print(str(brBook))
+    print(str(euaBook))
+
+    request.appendBook(brBook)
+    request.appendBook(euaBook)
+
+    print(request)
+    #TODO getUrls
+    #TODO for url in urls:
+    #    soup = openUrlAndSoup(urlToParse)
+    #    anchorAndGet(soup) TODO: return useful data
+    #    request.appendBook()
+
+    #TODO parse Request to workbook
+
+    #wb =
+    #TODO save workbook
 
 if __name__ == "__main__":
     main()
